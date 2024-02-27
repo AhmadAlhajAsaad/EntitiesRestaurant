1f58dfda0285eae21b6325ad6ba56800248c43f8
Ahmad_Alhaj_Asaad <79484313+82Ahmad82@users.noreply.github.com>
2024-02-27 15:00:45 +0100

    first commit

---
+++ Entities.md
@@ -0,0 +1,97 @@
+﻿# Entity Class Summary
+
+The `Entity` class is an abstract base class designed for all entities in the system. It enforces consistent properties across entities and includes fields for creation and modification tracking, as well as concurrency control. The key properties are:
+
+- **Id:** Unique identifier for the entity.
+- **CreatedAt:** Timestamp of entity creation.
+- **CreatedById:** Unique identifier of the creator. 
+- **CreatedByName:** Name of the creator (defaulted to an empty string).
+- **UpdatedAt:** Timestamp of the last update.
+- **UpdatedById:** Unique identifier of the last updater.
+- **UpdatedByName:** Name of the last updater (defaulted to an empty string).
+
+Being abstract, it cannot be instantiated independently and serves as a foundation for specific entity types.
+
+# Entity Creation Workflow - ExampleEntity
+
+The creation workflow for the `ExampleEntity` class is summarized below:
+
+## 1. Inheritance from `Entity`:
+```csharp
+/// <summary>
+/// Because it inherits from the Entity class, this class has the properties CreatedAt, CreatedById, CreatedByName, UpdatedAt, UpdatedById, UpdatedByName.
+/// </summary>
+public class ExampleEntity : Entity
+{
+    
+}
+```
+
+## 2. Define Entity Properties:
+```csharp
+// Define properties of the entity
+public string Name { get; set; }
+public string Description { get; set; }
+public decimal Price { get; set; }
+```
+
+## 3. Define Entity Constructor:
+```csharp
+// Define a constructor for the entity
+public ExampleEntity(string name, string description, decimal price)
+{
+    Name = name;
+    Description = description;
+    Price = price;
+}
+```
+
+## 4. Define Entity Factory Method for `ef core`:
+```csharp
+// Define a private constructor for ef core
+private ExampleEntity()
+{
+    // needed for ef core
+}
+
+```
+
+## 5. Create Domain specific methods when needed:
+```csharp
+// Define domain specific methods
+public void Update(string name, string description, decimal price)
+{
+    Name = name;
+    Description = description;
+    Price = price;
+}
+```
+## 6. Review the Entity Class:
+```csharp
+// Review the entity class
+public class ExampleEntity : Entity
+{
+    private ExampleEntity()
+    {
+        // needed for ef core
+    }
+    
+    public ExampleEntity(string name, string description, decimal price)
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+    }
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    
+    public void Update(string name, string description, decimal price)
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+    }
+}
+```
---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class RestaurantDeliveryInformation
+{
+    public Guid ID { get; set; } // PK
+    public double MinimumOrderCost { get; set; }
+    
+    [ForeignKey("ZeroFeeAmount")]
+    public double ZeroFeeAmount { get; set; } // FK
+    
+    [ForeignKey("DeliveryFee")]
+    public double DeliveryFee { get; set; } // FK
+
+    // Navigation property
+    public Restaurant Restaurant { get; set; }
+}
+
+
+public class Restaurant
+{
+    public Guid Id { get; set; } // PK
+    public string Name { get; set; }
+    public Location Location { get; set; } // Navigation property
+    public OpeningHours OpeningHours { get; set; } // Navigation property
+    public string Category { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Description { get; set; }
+    public RestaurantDeliveryInformation RestaurantDeliveryInformation { get; set; } // Navigation property
+}
+
+
+public class Location
+{
+    public Guid ID { get; set; } // PK
+    public string StreetName { get; set; }
+    public string Zipcode { get; set; }
+    public string StreetNumber { get; set; }
+    public string City { get; set; }
+}
+
+
+public class OpeningHours
+{
+    public Guid ID { get; set; } // PK
+    public DateTime TimeFrom { get; set; }
+    public DateTime TimeTo { get; set; }
+    public WeekDay Weekday { get; set; }
+}
+
+public enum WeekDay
+{
+    Monday, Tuesday,  Wednesday, Thursday, Friday, Saturday, Sunday
+}
+
+
+
+public class Menu
+{
+    public Guid ID { get; set; } // PK
+    public Guid RestaurantID { get; set; } // FK
+    public string Name { get; set; }
+    public ICollection<MenuItem> MenuItems { get; set; } // Navigation property
+    public Restaurant Restaurant { get; set; } // Navigation property
+}
+
+public class MenuItem
+{
+    public Guid ID { get; set; } // PK
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public Guid RestaurantID { get; set; } // FK
+    public string ImageUrl { get; set; }
+    public string Description { get; set; }
+    public MenuCategory MenuCategory { get; set; } // Navigation property
+}
+
+public class MenuCategory
+{
+    public Guid ID { get; set; } // PK
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Guid MenuItemId { get; set; } // FK
+}
+
+
+public class Order
+{
+    public Guid ID { get; set; } // PK
+    public OrderStatusEnum OrderStatus { get; set; }
+    public Courier Courier { get; set; } // Navigation property
+    public Location PickupLocation { get; set; } // Navigation property
+    public Location DeliveryLocation { get; set; } // Navigation property
+    public ICollection<MenuItem> MenuItems { get; set; } // Navigation property
+    public decimal TotalPrice { get; set; }
+    public string Note { get; set; }
+}
+
+
+public enum OrderStatusEnum
+{
+    OrderPlaced, OrderAssignedToCourier, CourierDelivering, OrderDelivered
+}
+
+
+public class OrderAssignedToCourier
+{
+    public Guid OrderPlacedId { get; set; } // FK
+    public Guid OrderAssignedToCourierId { get; set; } // FK
+    public Guid CourierDeliveringId { get; set; } // FK
+    public Guid OrderDeliveredId { get; set; } // FK
+}
+
+
+
+public class User
+{
+    public Guid ID { get; set; } // PK
+    public string Username { get; set; }
+    public UserType UserType { get; set; } // FK
+}
+
+
+public class Customer
+{
+    public Guid ID { get; set; } // PK
+    public Guid UserId { get; set; } // PK, FK
+    public Guid LocationId { get; set; } // FK
+    public Guid OrderID { get; set; } // FK
+    public string PhoneNumber { get; set; }
+}
+
+
+public class RestaurantAdmin
+{
+    public Guid ID { get; set; } // PK
+    public Guid UserId { get; set; } // PK, FK
+}
+
+
+public enum UserType
+{  Admin, Customer, RestaurantAdmin, Courier}
+
+
+public class Courier
+{
+    public Guid ID { get; set; } // PK
+    public Guid UserId { get; set; } // PK, FK
+    public bool OnDuty { get; set; }
+    public OnDutyStatus OnDutyStatus { get; set; }
+    public string PhoneNumber { get; set; }
+}
+
+
+public enum OnDutyStatus
+{  Available, Busy, Offline }
